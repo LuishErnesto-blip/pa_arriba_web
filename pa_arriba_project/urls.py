f491d9c0e81c20988cd945cb6eedd6a945752b61
Luis Ernesto <luis.bracerog06@gmail.com>
2025-11-18 11:13:25 -0500

    Activando SEO robots y sitemap final

--- pa_arriba_project/urls.py
+++ pa_arriba_project/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# --- IMPORTACIÓN CLAVE PARA SEO ---
+# Traemos las funciones que creaste en core/views.py para que Google las vea
+from core.views import robots_txt, sitemap_xml 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
@@ -12,6 +16,11 @@ urlpatterns = [
     
     # Ruta Estratégica para el Test de Caos
     path('diagnostico/', include('termometro.urls')),
+
+    # --- RUTAS SEO PARA GOOGLE ---
+    # Esto permite que funcionen tusitio.com/robots.txt y tusitio.com/sitemap.xml
+    path('robots.txt', robots_txt, name='robots_txt'),
+    path('sitemap.xml', sitemap_xml, name='sitemap_xml'),
 ]
 
 if settings.DEBUG:
